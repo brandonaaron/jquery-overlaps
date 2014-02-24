ba936fcc0f111d500be6d1a2bf85e65a09364a1e
Brooks Ryba <brooks@sightmachine.com>
2014-02-24 12:44:29 -0500

    Update jquery.overlaps.js

--- jquery.overlaps.js
+++ jquery.overlaps.js
@@ -14,11 +14,13 @@
     }
 }(function ($) {
 
-    $.fn.overlaps = function(selector) {
-        return this.pushStack(filterOverlaps(this, selector && $(selector)));
+    $.fn.overlaps = function(options) {
+        if( options == undefined ) { options = {} }
+        if( options.padding == undefined ) { options.padding = 0; }
+        return this.pushStack(filterOverlaps(this, options.selector && $(options.selector), options));
     };
 
-    function filterOverlaps(collection1, collection2) {
+    function filterOverlaps(collection1, collection2, options) {
         var dims1  = getDims(collection1),
             dims2  = !collection2 ? dims1 : getDims(collection2),
             stack  = [],
@@ -33,7 +35,7 @@
             for (index2 = 0; index2 < length2; index2++) {
                 if (collection1[index1] === collection2[index2]) {
                     continue;
-                } else if (checkOverlap(dims1[index1], dims2[index2])) {
+                } else if (checkOverlap(dims1[index1], dims2[index2], options)) {
                     stack.push( (length1 > length2) ?
                         collection1[index1] :
                         collection2[index2]);
@@ -49,22 +51,27 @@
 
         while ((elem = elems[i++])) {
             offset = $(elem).offset();
+            box = elem.getBoundingClientRect();
+
             dims.push([
                 offset.top,
                 offset.left,
-                elem.offsetWidth,
-                elem.offsetHeight
+                elem.offsetWidth || box.width,
+                elem.offsetHeight || box.height
             ]);
         }
 
         return dims;
     }
 
-    function checkOverlap(dims1, dims2) {
-        var x1 = dims1[1], y1 = dims1[0],
-            w1 = dims1[2], h1 = dims1[3],
-            x2 = dims2[1], y2 = dims2[0],
-            w2 = dims2[2], h2 = dims2[3];
+    function checkOverlap(dims1, dims2, options) {
+        padding = options.padding 
+
+        var x1 = dims1[1] - padding, y1 = dims1[0] - padding,
+            w1 = dims1[2] + padding, h1 = dims1[3] + padding,
+            x2 = dims2[1] - padding, y2 = dims2[0] - padding,
+            w2 = dims2[2] + padding, h2 = dims2[3] + padding;
+
         return !(y2 + h2 <= y1 || y1 + h1 <= y2 || x2 + w2 <= x1 || x1 + w1 <= x2);
     }
 
